443f870fc9680ee5c74d37927078cc491b32dad7
LiterallyANoodle <themaskedkid321@gmail.com>
2024-04-09 18:49:38 -0500

    tables without constraints

--- Assignment2-A.sql
+++ Assignment2-A.sql
@@ -55,7 +55,7 @@ CREATE TABLE Port_visit (
 # define relationship tables 
 CREATE TABLE History (
 	Time_stamp datetime NOT NULL,
-    Sname varchar(255) UNIQUE,
+    Sname varchar(255),
     
     PRIMARY KEY (Time_stamp, Sname),
     FOREIGN KEY (Time_stamp) REFERENCES Ship_movement(Time_stamp),
@@ -64,9 +64,47 @@ CREATE TABLE History (
 
 CREATE TABLE Ship_of_type (
 	Sname varchar(255) NOT NULL,
-    Type varchar(255) UNIQUE,
+    Type varchar(255),
     
     PRIMARY KEY (Sname, Type),
     FOREIGN KEY (Sname) REFERENCES Ship(Sname),
     FOREIGN KEY (Type) REFERENCES Ship_type(Type)
+);
+
+CREATE TABLE Home_port (
+	Sname varchar(255),
+    Pname varchar(255),
+    
+    PRIMARY KEY (Sname, Pname),
+    FOREIGN KEY (Sname) REFERENCES Ship(Sname),
+    FOREIGN KEY (Pname) REFERENCES Port(Pname)
+);
+
+CREATE TABLE Ship_at_port (
+	Start_date datetime NOT NULL,
+    Sname varchar(255),
+    Pname varchar(255),
+    
+    PRIMARY KEY (Start_date, Sname, Pname),
+    FOREIGN KEY (Start_date) REFERENCES Port_visit(Start_date),
+    FOREIGN KEY (Sname) REFERENCES Ship(Sname),
+    FOREIGN KEY (Pname) REFERENCES Port(Pname)
+);
+
+CREATE TABLE Port_in_state_country (
+	Pname varchar(255) NOT NULL,
+    SCName varchar(255),
+    
+    PRIMARY KEY (Pname, SCName),
+    FOREIGN KEY (Pname) REFERENCES Port(Pname),
+    FOREIGN KEY (SCName) REFERENCES State_country(Name)
+);
+
+CREATE TABLE Port_on_sea_ocean_lake (
+	Pname varchar(255) NOT NULL,
+    SOLName varchar(255),
+    
+    PRIMARY KEY (Pname, SOLName),
+    FOREIGN KEY (Pname) REFERENCES Port(Pname),
+    FOREIGN KEY (SOLName) REFERENCES Sea_ocean_lake(Name)
 );
